ab97f0c88d19a5e0f5784ab0b6847b8e0bd362b4
78-artilleryman <ywy040150@gmail.com>
2024-03-17 18:34:47 +0900

    feat: 카카오맵 컴포넌트 생성
    - 카카오 맵 api를 이용해 컴포넌트 생성

---
+++ src/components/Map.tsx
@@ -0,0 +1,35 @@
+/* global kakao*/
+import React from 'react';
+import Script from 'next/script';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+function Map() {
+  const loadKakaMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById('map');
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      new window.kakao.maps.Map(mapContainer, mapOption);
+    });
+  };
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaMap}
+      />
+      <div id="map" className="w-full h-screen"></div>
+    </>
+  );
+}
+
+export default Map;
