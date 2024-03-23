da0b978e436cc5763178447d756193bff4b534f5
78-artilleryman <ywy040150@gmail.com>
2024-03-23 14:18:16 +0900

    refactor: react query를 이용해 데이터 가져오는 방식으로 변경

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -4,7 +4,11 @@ import { PrismaClient } from '@prisma/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<StoreType[]>) {
   const prisma = new PrismaClient();
-  const stores = await prisma.store.findMany();
+  const stores = await prisma.store.findMany({
+    orderBy: {
+      id: 'asc',
+    },
+  });
 
   res.status(200).json(stores);
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -2,44 +2,58 @@ import { StoreType } from '@/interface';
 import Image from 'next/image';
 import React from 'react';
 import axios from 'axios';
+import { useQuery } from 'react-query';
+import Loding from '@/components/Loding';
 
-function StoreListPage({ stores }: { stores: StoreType[] }) {
+function StoreListPage() {
+  const {
+    isLoading,
+    isError,
+    data: stores,
+  } = useQuery('stores', async () => {
+    const { data } = await axios('/api/stores');
+    return data as StoreType[];
+  });
+
+  if (isError) {
+    // 로딩중일때 보여줄 결과
+    return (
+      <span className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">다시 시도해주세요</span>
+    );
+  }
+
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
       <ul role="list" className="divide-y divide-gray-100">
-        {stores?.map((store, index) => (
-          <li key={index} className="flex justify-between gap-x-6 py-5">
-            <div className="flex gap-x-4">
-              <Image
-                src={store?.category ? `/images/markers/${store?.category}.png` : `/images/markers/default.png`}
-                width={48}
-                height={48}
-                alt="아이콘 이미지"
-              />
-              <div>
-                <div className="text-sm font-semibold leading-6 text-gray-900">{store?.name}</div>
-                <div className="mt-1 text-sx text-sm font-semibold leading-5 text-gray-500">{store?.stroeType}</div>
+        {isLoading ? (
+          <Loding />
+        ) : (
+          stores?.map((store, index) => (
+            <li key={index} className="flex justify-between gap-x-6 py-5">
+              <div className="flex gap-x-4">
+                <Image
+                  src={store?.category ? `/images/markers/${store?.category}.png` : `/images/markers/default.png`}
+                  width={48}
+                  height={48}
+                  alt="아이콘 이미지"
+                />
+                <div>
+                  <div className="text-sm font-semibold leading-6 text-gray-900">{store?.name}</div>
+                  <div className="mt-1 text-sx text-sm font-semibold leading-5 text-gray-500">{store?.stroeType}</div>
+                </div>
               </div>
-            </div>
-            <div className="hidden sm:flex sm: flex-col sm: items-end">
-              <div className="text-sm font-semibold leading-6 text-gray-900">{store?.address}</div>
-              <div className="mt-1 text-sx text-sm font-semibold leading-5 text-gray-500">
-                {store.phone || '번호없음'} | {store?.foodCertifyName} | {store?.category || '구분없음'}
+              <div className="hidden sm:flex sm: flex-col sm: items-end">
+                <div className="text-sm font-semibold leading-6 text-gray-900">{store?.address}</div>
+                <div className="mt-1 text-sx text-sm font-semibold leading-5 text-gray-500">
+                  {store.phone || '번호없음'} | {store?.foodCertifyName} | {store?.category || '구분없음'}
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
 }
 
 export default StoreListPage;
-
-export async function getServerSideProps() {
-  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
-
-  return {
-    props: { stores: stores.data },
-  };
-}
