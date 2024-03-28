a5c761339a28ccbf879af0e5ce24edc9b657c663
78-artilleryman <ywy040150@gmail.com>
2024-03-28 17:10:00 +0900

    feat: naver 로그인 기능 구현

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,27 @@
+import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
-import NextAuth from 'next-auth/next';
+
 import GoogleProvider from 'next-auth/providers/google';
+import NaverProvider from 'next-auth/providers/naver';
 
 const prisma = new PrismaClient();
 
 export const authOptions = {
-  Adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || '',
+      clientSecret: process.env.NAVER_CLIENT_SECRET || '',
+    }),
   ],
+  pages: {
+    signIn: '/users/login',
+  },
 };
 
 export default NextAuth(authOptions);
--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AiOutlineGoogle } from 'react-icons/ai';
+import { SiNaver } from 'react-icons/si';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.replace('/');
+    }
+  }, [status, router]);
+
+  return (
+    <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
+      <div className="mx-auto w-full max-w-sm">
+        <div className="text-blue-800 text-center text-2xl font-semibold italic">FoodMap</div>
+      </div>
+      <div className="text-center mt-6 text-2xl font-bold text-gray-600">SNS 계정으로 로그인해주세요</div>
+      <p className="mt-2 text-center text-sm text-gray-600">계정이 없다면 자동으로 회원가입이 진행됩니다</p>
+      <div className="mt-10 mx-auto w-full max-w-sm">
+        <div className="flex flex-col gap-2">
+          <button
+            type="button"
+            className="text-white flex gap-3  bg-[#4285f4] hover:bg-[#4285f4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn('google')}
+          >
+            <AiOutlineGoogle className="w-6 h-6" />
+            Sign in with Google
+          </button>
+          <button
+            type="button"
+            className="text-white flex gap-4  bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn('naver')}
+          >
+            <SiNaver className="w-4 h-4" />
+            Sign in with Naver
+          </button>
+          <button
+            type="button"
+            className="text-black flex gap-3  bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn('kakao')}
+          >
+            <RiKakaoTalkFill className="w-6 h-6" />
+            Sign in with KaKao
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
