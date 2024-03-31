1a2ba19866513fe49851b2ee14481f68c21fb08b
78-artilleryman <ywy040150@gmail.com>
2024-04-01 00:08:46 +0900

    fix: Adapter type 문제 해결

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,31 @@
-import NextAuth from 'next-auth/next';
+import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import { Adapter } from 'next-auth/adapters';
 
-import GoogleProvider from 'next-auth/providers/google';
-import NaverProvider from 'next-auth/providers/naver';
-import KakaoProvider from 'next-auth/providers/kakao';
+import Google from 'next-auth/providers/google';
+import Naver from 'next-auth/providers/naver';
+import Kakao from 'next-auth/providers/kakao';
 
 const prisma = new PrismaClient();
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   session: {
     strategy: 'jwt' as const,
     maxAge: 60 * 60 * 24,
     updateAge: 60 * 60 * 2,
   },
-  adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
+    Google({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
-    NaverProvider({
+    Naver({
       clientId: process.env.NAVER_CLIENT_ID || '',
       clientSecret: process.env.NAVER_CLIENT_SECRET || '',
     }),
-    KakaoProvider({
+    Kakao({
       clientId: process.env.KAKAO_CLIENT_ID || '',
       clientSecret: process.env.KAKAO_CLIENT_SECRET || '',
     }),
